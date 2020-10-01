710f619bca2058af73b8dcacd15f5460472bb66b
AgungSetyaPratama <agungwas@gmail.com>
2020-10-02 02:38:46 +0700

    1

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const Express = require('express')
 const app = Express()
-
+let data = 'https://api.nanopool.org/v1/eth/workers/0x20aa46b632b7ca6f0c7dbd91e0bc7b1c7215a0ef'
+JSON.parse(data)
+console.log(data)
 app.use('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    let data = 'https://api.nanopool.org/v1/eth/workers/0x20aa46b632b7ca6f0c7dbd91e0bc7b1c7215a0ef'
+    res.render('index', {data} )
+})
